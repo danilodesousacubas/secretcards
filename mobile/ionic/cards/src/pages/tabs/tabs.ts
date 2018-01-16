6acfa984c1544745968bfd6cecd72d1842789d26
Danilo de Souza <danilodesousa@gmail.com>
2018-01-16 20:45:04 -0200

    up project cards in ionic

---
+++ mobile/ionic/cards/src/pages/tabs/tabs.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+import { AboutPage } from '../about/about';
+import { ContactPage } from '../contact/contact';
+import { HomePage } from '../home/home';
+import { LoginPage } from '../login/login';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab0Root = LoginPage;
+  tab1Root = HomePage;
+  tab2Root = AboutPage;
+  tab3Root = ContactPage;
+
+  constructor() {
+
+  }
+}
