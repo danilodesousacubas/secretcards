92d93428c29932950d387736d7f5c0fd59c8d66c
Danilo de Souza <danilodesousa@gmail.com>
2018-01-25 22:14:12 -0200

    Add config qas

--- frontend/src/main/frontend/src/app/config/config.ts
+++ frontend/src/main/frontend/src/app/config/config.ts
@@ -4,20 +4,12 @@ export class Config{
     private _perfil = 'production';
     
     constructor() {
-        
-        console.log("=============================");
-        console.log("=============================" + this._perfil);
-        
-        
         if(this._perfil === 'local'){
-            
-            console.log("SET LOCAL");
             this._context = 'http://localhost:8080';
-        }
-
-        if(this._perfil === 'production'){
-            console.log("SET PRODUCTION");
+        } else if(this._perfil === 'production'){
             this._context = 'https://secretcards.herokuapp.com';
+        } else if(this._perfil === 'qas'){
+            this._context = 'https://qas-secretcards.herokuapp.com';
         }
     }
 
