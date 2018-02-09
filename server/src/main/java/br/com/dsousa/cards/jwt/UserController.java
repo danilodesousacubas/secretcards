ffdc5f43159c878cb57866428e3c9a967e16e470
Danilo de Souza <danilodesousa@gmail.com>
2018-02-08 22:27:30 -0200

    curreset name

--- server/src/main/java/br/com/dsousa/cards/jwt/UserController.java
+++ server/src/main/java/br/com/dsousa/cards/jwt/UserController.java
@@ -1,5 +1,8 @@
 package br.com.dsousa.cards.jwt;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +14,16 @@ public class UserController {
 	@RequestMapping("/users")
 	@ResponseBody
 	public String getUsers(){
+		
+		
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (!(authentication instanceof AnonymousAuthenticationToken)) {
+		    String currentUserName = authentication.getName();
+		    return currentUserName;
+		}
+	
+		
+		
 		return "{\"users\":[{\"name\":\"Lucas\", \"country\":\"Brazil\"}," +
 		           "{\"name\":\"Jackie\",\"country\":\"China\"}]}";
 	
